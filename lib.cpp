7d42929d6719ec1746b8a27e0b32d6d4da57eec9
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2021-12-11 14:33:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+//
+// Created by Valentina on 11/12/2021.
+//
+#include "lib.h"
+float numM (const float num[], int dim){
+    num[dim];
+    float max = num[0];
+    for (int i = 0; i < dim; ++i) {
+        if(max<num[i]){
+            max=num[i];
+        }
+
+    }
+    return max;
+}
 
